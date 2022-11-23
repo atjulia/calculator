5a762cda152c22847461757f01156fdc07054b7c
Julia Karolina <juliakarolina24@gmail.com>
2022-11-23 15:37:07 -0300

    calculator

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from './main/Calculator'
+export default {
+  components: { Calculator }
+
+}
+</script>
+
+<style>
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  background: aliceblue;
+}
+
+</style>
